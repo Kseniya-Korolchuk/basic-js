b0c57b0c404bc4d44ba0136f2f4d1c18dc8707ee
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-05-02 22:45:51 +0300

    feat: vigenere-cipher without double-sided reverse

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+
+  isLetter (str) {
+    return str.length === 1 && str.match(/[a-zA-Z]/i);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  isUpperCase (character) {
+    if (character === character.toUpperCase()) {
+      return true;
+    }
+    if (character === character.toLowerCase()) {
+      return false;
+    }
   }
-}
+
+  encrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+      for (let i = 0, j = 0; i < message.length; i++) {
+        const c = message.charAt(i);
+        if (this.isLetter(c)) {
+            result += String.fromCharCode((c.charCodeAt(0) + key.charCodeAt(j) - 2 * 65) % 26 + 65);// A: 65
+            j = ++j % key.length;
+          } 
+        else {
+          result += c;
+        }
+        //j = ++j % key.length;
+      }
+      return result;
+  }
+
+  decrypt(message, key) {
+    //throw new NotImplementedError('Not implemented');
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const c = message.charAt(i);
+      if (this.isLetter(c)) {
+          result += String.fromCharCode(90 - (25 - (c.charCodeAt(0) - key.charCodeAt(j))) % 26);
+          j = ++j % key.length;
+      } else {
+        result += c;
+      }
+      
+    }
+    return result;
+    }
+  }
+    const messageEncrypt = new VigenereCipheringMachine;
+    
+    /*encrypt('Hello World!', 'code')*/
+    console.log(messageEncrypt.decrypt('HSVD AJAL ^^', 'behappy'))
+
+
 
 module.exports = {
   VigenereCipheringMachine
